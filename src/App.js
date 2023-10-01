660236322c1af5c8941227c9059dc27ef36296ed
Nayan Kumar Jha <nayankumarjha2@gmail.com>
2023-10-02 00:16:44 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 
 
-const API_URL ='http://www.omdbapi.com?i=tt3896198&apikey=35448109';
+const API_URL ='https://www.omdbapi.com?i=tt3896198&apikey=35448109';
 
 
 
